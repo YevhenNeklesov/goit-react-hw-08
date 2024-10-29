1ad2c80ef85757f228ef93190a2d9b73bdc03e40
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-10-29 17:44:19 +0100

    update loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
 
-import s from './Loader.module.css'
+
 
 const Loader = () => {
   return (
-      <div className={s.loader}>
-
+      <div >
+            <span className="loading absolute left-2/4 top-2/4 loading-ring loading-lg"></span>
     </div>
   )
 }
